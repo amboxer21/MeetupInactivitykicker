fe10ce3dd3cc555d5b9c9aa879671d320802fe7d
amboxer21 <amboxer21@gmail.com>
2019-07-09 10:58:03 -0400

    Big code refactor.

--- src/meetupInactivitykicker.py
+++ src/meetupInactivitykicker.py
@@ -19,36 +19,65 @@ class MetaMeetupInactivitykicker(type):
             cls.count = 0
         if not hasattr(cls,'member_list'):
             cls.member_list = {}
-        if not hasattr(cls,'member_name'):
-            cls.member_name = None
+        if not hasattr(cls,'site_name'):
+            cls.site_name = None
+        if not hasattr(cls,'driver'):
+            user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0'
+            profile    = webdriver.FirefoxProfile()
+            profile.set_preference("general.useragent.override", user_agent)
+            cls.driver = webdriver.Firefox(profile)
+
 
 class MeetupInactivitykicker(object):
 
     __metaclass__ = MetaMeetupInactivitykicker
 
     def __init__(self,config_dict={}):
-        self.site_name    = None
         self.email        = config_dict['email'] 
         self.message      = config_dict['message']
+        self.dry_run      = config_dict['dry_run']
         self.password     = config_dict['password']
         self.group_name   = config_dict['group_name']
         self.send_message = config_dict['send_message']
-        user_agent        = 'Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0'
-        profile           = webdriver.FirefoxProfile()
-        profile.set_preference("general.useragent.override", user_agent)
-        self.driver       = webdriver.Firefox(profile)
-        self.driver.get("https://secure.meetup.com/login")
+
+    def populate_member_dict(self, elements):
+        for element in elements:
+            href = element.get_attribute("href")
+            member = re.search('https://www.meetup.com/'
+                + str(MeetupInactivitykicker.site_name)
+                + 'members/(\d+)/profile/.*', href, re.M | re.I)
+            if member is not None:
+                MeetupInactivitykicker.member_list[member.group(1)] = member.group()
+
+    def navigate_to_members_page(self, elements):
+        for element in elements:
+            try:
+                href = element.get_attribute("href")
+                res  = re.findall('https://www.meetup.com/*'
+                    + str(self.group_name)
+                    + '.*', href, re.I | re.M)
+                if res:
+                    MeetupInactivitykicker.count += 1
+                    if MeetupInactivitykicker.count > 1:
+                        print("The group name you've provided is to vage")
+                        sys.exit(1)
+                    MeetupInactivitykicker.count += 1
+                MeetupInactivitykicker.driver.get(str(res[0])+'/members')
+                MeetupInactivitykicker.site_name = re.sub('https://www.meetup.com/', '', res[0])
+            except Exception as exception:
+                pass
 
     def remove_member(self, member):
-        self.driver.find_element_by_css_selector("svg.svg.svg--overflow.svg-icon.valign--middle").click()
-        self.driver.find_element_by_class_name('member-actions-menuItem-remove_member.dropdownMenu-item.display--flex.span--100').click()
+        MeetupInactivitykicker.driver.find_element_by_css_selector("svg.svg.svg--overflow.svg-icon.valign--middle").click()
+        MeetupInactivitykicker.driver.find_element_by_class_name('member-actions-menuItem-remove_member.dropdownMenu-item.display--flex.span--100').click()
         if self.send_message:
-            text_box = self.driver.find_element_by_css_selector('textarea#removeMemberModalTextarea').send_keys(self.message)
-        self.driver.find_element_by_xpath("//span[text()='Remove member']").click()
+            text_box = MeetupInactivitykicker.driver.find_element_by_css_selector('textarea#removeMemberModalTextarea').send_keys(self.message)
+        if not self.dry_run:
+            MeetupInactivitykicker.driver.find_element_by_xpath("//span[text()='Remove member']").click()
 
     def active(self, status):
         try:
-            status = self.driver.find_element_by_xpath("//span[contains(text(),"+str(status)+")]").text
+            status = MeetupInactivitykicker.driver.find_element_by_xpath("//span[contains(text(),"+str(status)+")]").text
             if 'no' in status:
                 regex = re.search('\d+', status, re.M | re.I).group()
             if 'yes' in status:
@@ -60,52 +89,32 @@ class MeetupInactivitykicker(object):
             return False
 
     def login(self):
-        uname = self.driver.find_element_by_name("email")
+        MeetupInactivitykicker.driver.get("https://secure.meetup.com/login")
+        uname = MeetupInactivitykicker.driver.find_element_by_name("email")
         uname.send_keys(self.email)
-        pword = self.driver.find_element_by_name("password")
+        pword = MeetupInactivitykicker.driver.find_element_by_name("password")
         pword.send_keys(self.password,Keys.RETURN)
         time.sleep(5)
 
     def main(self):
         try:
-            self.driver.find_element_by_class_name('simple-view-selector-anc').click()
+            MeetupInactivitykicker.driver.find_element_by_class_name('simple-view-selector-anc').click()
         except NoSuchElementException as noSuchElementException:
             print('Exception NoSuchElementException => '+str(noSuchElementException))
             sys.exit(1)
-        elems = self.driver.find_elements_by_xpath("//a[@href]")
-        for elem in elems:
-            try:
-                href = elem.get_attribute("href")
-                res  = re.findall('https://www.meetup.com/*'+str(self.group_name)+'.*', href, re.I | re.M)
-                if res:
-                    MeetupInactivitykicker.count += 1
-                    if MeetupInactivitykicker.count > 1:
-                        print("The group name you've provided is to vage")
-                        sys.exit(1)
-                    MeetupInactivitykicker.count += 1
-                self.driver.get(str(res[0])+'/members')
-                self.site_name = re.sub('https://www.meetup.com/', '', res[0])
-            except Exception as exception:
-                pass
-        elems = self.driver.find_elements_by_xpath("//a[@href]")
-        for elem in elems:
-            href = elem.get_attribute("href")
-            member = re.search('https://www.meetup.com/'+str(self.site_name)+'members/(\d+)/profile/.*', href, re.M | re.I)
-            if member is not None:
-                MeetupInactivitykicker.member_list[member.group(1)] = member.group()
+        self.navigate_to_members_page(MeetupInactivitykicker.driver.find_elements_by_xpath("//a[@href]"))
+        self.populate_member_dict(MeetupInactivitykicker.driver.find_elements_by_xpath("//a[@href]"))
         for idno, member in sorted(MeetupInactivitykicker.member_list.items()):
-            self.driver.get(str(member))
+            MeetupInactivitykicker.driver.get(str(member))
             del MeetupInactivitykicker.member_list[idno]
             try:
-                #none = self.driver.find_element_by_xpath("//span[contains(text(), 'None')]").text
                 if self.active('None') or self.active('yes'):
-                    #remove from group
-                    print('Removing '+str(idno)+' from group!')
+                    print('Removing member with idno: '+str(idno)+' from group!')
                     self.remove_member(member)
                 try:
-                    self.driver.back()
+                    MeetupInactivitykicker.driver.back()
                 except:
-                    self.driver.execute_script("window.history.go(-1)")
+                    MeetupInactivitykicker.driver.execute_script("window.history.go(-1)")
                     pass
             except :
                 pass
@@ -130,6 +139,9 @@ if __name__ == '__main__':
     parser.add_option('-t', '--timeout',
         dest='timeout',type='int',
         help='This options sets the time in between each retry.')
+    parser.add_option('-d', '--dry-run',
+        dest='dry_run',action='store_true', default=False,
+        help='This options runs the script without deleting members.')
     parser.add_option('-s', '--send-message',
         dest='send_message', action='store_true', default=False,
         help='This options sends the default message if a custom message isnt passed.')
@@ -143,6 +155,7 @@ if __name__ == '__main__':
       'email'        : options.email,
       'message'      : options.message,
       'timeout'      : options.timeout,
+      'dry_run'      : options.dry_run,
       'password'     : options.password,
       'group_name'   : options.group_name,
       'send_message' : options.send_message
