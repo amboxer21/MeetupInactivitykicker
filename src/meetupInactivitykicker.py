b207bf35f76fad9e5b31b03341806cbbe82d38af
amboxer21 <amboxer21@gmail.com>
2019-07-11 15:53:39 -0400

    Added more code to base.

--- src/meetupInactivitykicker.py
+++ src/meetupInactivitykicker.py
@@ -33,12 +33,21 @@ class MeetupInactivitykicker(object):
     __metaclass__ = MetaMeetupInactivitykicker
 
     def __init__(self,config_dict={}):
-        self.email        = config_dict['email'] 
-        self.message      = config_dict['message']
-        self.dry_run      = config_dict['dry_run']
-        self.password     = config_dict['password']
-        self.group_name   = config_dict['group_name']
-        self.send_message = config_dict['send_message']
+        self.email                = config_dict['email'] 
+        self.dry_run              = config_dict['dry_run']
+        self.password             = config_dict['password']
+        self.group_name           = config_dict['group_name']
+        self.removal_message      = config_dict['removal_message']
+        self.warning_message      = config_dict['warning_message']
+        self.send_removal_message = config_dict['send_removal_message']
+        self.send_warning_message = config_dict['send_warning_message']
+
+    def back(self):
+        try:
+            MeetupInactivitykicker.driver.back()
+        except:
+            MeetupInactivitykicker.driver.execute_script("window.history.go(-1)")
+            pass
 
     def populate_member_dict(self, elements):
         for element in elements:
@@ -70,10 +79,24 @@ class MeetupInactivitykicker(object):
     def remove_member(self):
         MeetupInactivitykicker.driver.find_element_by_css_selector("svg.svg.svg--overflow.svg-icon.valign--middle").click()
         MeetupInactivitykicker.driver.find_element_by_class_name('member-actions-menuItem-remove_member.dropdownMenu-item.display--flex.span--100').click()
-        if self.send_message:
-            text_box = MeetupInactivitykicker.driver.find_element_by_css_selector('textarea#removeMemberModalTextarea').send_keys(self.message)
+        if self.send_removal_message:
+            MeetupInactivitykicker.driver.find_element_by_css_selector('textarea#removeMemberModalTextarea').send_keys(self.removal_message)
         if not self.dry_run:
+            print('Removing '
+                + str(MeetupInactivitykicker.member_name)
+                + ' from group!')
             MeetupInactivitykicker.driver.find_element_by_xpath("//span[text()='Remove member']").click()
+
+    def warn_member(self):
+        MeetupInactivitykicker.driver.find_element_by_css_selector("a.text--small.messageButton").click()
+        if self.send_warning_message:
+            MeetupInactivitykicker.driver.find_element_by_css_selector('textarea#messaging-new-convo.composeBox-textArea').send_keys(self.warning_message)
+        if not self.dry_run:
+            print('Sending '
+                + str(MeetupInactivitykicker.member_name)
+                + ' a warning message!')
+            MeetupInactivitykicker.driver.find_element_by_class_name('composeBox-sendButton.j-messageSend.messagingButton').send_keys(Keys.RETURN)
+        self.back()
 
     def active(self, status):
         try:
@@ -113,21 +136,16 @@ class MeetupInactivitykicker(object):
                 if self.active('None') or self.active('yes'):
                     member_name_regex = "//div[@class='flex-item']/p[@class='text--big text--bold']"
                     MeetupInactivitykicker.member_name = MeetupInactivitykicker.driver.find_element_by_xpath(member_name_regex).text
-                    print('Removing '
-                        + str(MeetupInactivitykicker.member_name)
-                        + ' from group!')
-                    self.remove_member()
-                try:
-                    MeetupInactivitykicker.driver.back()
-                except:
-                    MeetupInactivitykicker.driver.execute_script("window.history.go(-1)")
-                    pass
+                    #self.remove_member()
+                    self.warn_member()
+                self.back()
             except :
                 pass
 
 if __name__ == '__main__':
 
-    message = 'You have been inactive in this group and are being removed by automated software.'
+    removal_message = 'You have been inactive in this group and are being removed by automated software.'
+    warning_message = 'You have been inactive in this group and will be removed if the inactivity continues.'
 
     parser = OptionParser()
     parser.add_option('-e', '--email',
@@ -139,7 +157,7 @@ if __name__ == '__main__':
     parser.add_option('-k', '--keyword',
         dest='group_name',
         help='This is the group name you would like to search on Meetup.com.')
-    parser.add_option('-r', '--retry',
+    parser.add_option('-R', '--retry',
         dest='retry', action="store_true", default=False,
         help='This options ensures that this program will try again if it cannot login.')
     parser.add_option('-t', '--timeout',
@@ -148,23 +166,31 @@ if __name__ == '__main__':
     parser.add_option('-d', '--dry-run',
         dest='dry_run',action='store_true', default=False,
         help='This options runs the script without deleting members.')
-    parser.add_option('-s', '--send-message',
-        dest='send_message', action='store_true', default=False,
-        help='This options sends the default message if a custom message isnt passed.')
-    parser.add_option('-m', '--message',
-        dest='message', default=message,
-        help='This options sends a custom message.')
+    parser.add_option('-S', '--send-removal-message',
+        dest='send_removal_message', action='store_true', default=False,
+        help='This options sends the default removal message if a custom message isnt passed.')
+    parser.add_option('-s', '--send-warning-message',
+        dest='send_warning_message', action='store_true', default=False,
+        help='This options sends the default warning message if a custom message isnt passed.')
+    parser.add_option('-r', '--removal-message',
+        dest='removal_message', default=removal_message,
+        help='This options sends a custom removal message.')
+    parser.add_option('-w', '--warning-message',
+        dest='warning_message', default=warning_message,
+        help='This options sends a custom warning message.')
     (options, args) = parser.parse_args()
 
     config_dict = {
-      'retry'        : options.retry,
-      'email'        : options.email,
-      'message'      : options.message,
-      'timeout'      : options.timeout,
-      'dry_run'      : options.dry_run,
-      'password'     : options.password,
-      'group_name'   : options.group_name,
-      'send_message' : options.send_message
+      'retry'                : options.retry,
+      'email'                : options.email,
+      'timeout'              : options.timeout,
+      'dry_run'              : options.dry_run,
+      'password'             : options.password,
+      'group_name'           : options.group_name,
+      'removal_message'      : options.removal_message,
+      'warning_message'      : options.warning_message,
+      'send_removal_message' : options.send_removal_message,
+      'send_warning_message' : options.send_warning_message,
     }
 
     meetupInactivityKicker = MeetupInactivitykicker(config_dict)
